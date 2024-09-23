4711f8f063e1448676de2ab7155988333fad2188
JW148 <47108195+JW148@users.noreply.github.com>
2024-09-23 11:52:37 +0100

    NextUI setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="flex m-8 justify-center">
+    <div className="flex flex-col m-8 text-center">
       <p className="text-9xl">Home</p>
+      <Button>Press ME</Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
