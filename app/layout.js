456ceb94f78802526670dbfe9d2df66c728d8724
jw148 <wiggin472@gmail.com>
2024-10-10 12:49:36 +0100

    Mobile responsiveness  - home header  - sidenav  - content header  - content cards  - content pages (Resource & Training WIP)

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./lib/providers";
-import Footer from "./components/ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
